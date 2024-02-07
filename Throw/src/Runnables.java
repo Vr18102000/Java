b14d51a504b239436a76d38803ad93db36aa109b
Vishal Rakhecha <rakhecha.vishal18@gmail.com>
2024-02-07 23:47:18 +0530

    Lambda used

--- Throw/src/Runnables.java
+++ Throw/src/Runnables.java
@@ -1,42 +1,32 @@
 
-class Aa implements Runnable
-{
-	public void run()
-	{
-		for(int i=1; i<=5;i++)
-		{
-			System.out.println("hi");
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-								e.printStackTrace();
-			}		//waiting stage like for 10 seconds
-		}
-	}
-}
-
-class B implements Runnable
-{
-	public void run()
-	{
-		for(int i=1; i<=5;i++)
-		{
-			System.out.println("hello");
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-								e.printStackTrace();
-			}		//waiting stage like for 10 seconds
-		}
-	}
-}
-
 public class Runnables {
 
 	public static void main(String[] args) {
 		
-		Runnable obj1 = new Aa();
-		Runnable obj2 = new B();
+		Runnable obj1 = () ->
+		{
+			for(int i=1; i<=5;i++)
+			{
+				System.out.println("hi");
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+									e.printStackTrace();
+				}		//waiting stage like for 10 seconds
+			}
+		};
+		Runnable obj2 = () ->
+		{
+			for(int i=1; i<=5;i++)
+			{
+				System.out.println("hello");
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+									e.printStackTrace();
+				}		//waiting stage like for 10 seconds
+			}
+		};
 		
 		Thread t1 = new Thread(obj1);
 		Thread t2 = new Thread(obj2);
